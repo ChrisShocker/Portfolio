4b993390512edf16f402eb8798237b05567cab8f
ChrisShocker <fallenchris@live.com>
2022-06-08 12:45:32 -0700

    Add form to check license plate

--- My Portfolio/Applications/React/Vehicle-Safety-Alert/client/src/components/body/Form.jsx
+++ My Portfolio/Applications/React/Vehicle-Safety-Alert/client/src/components/body/Form.jsx
@@ -5,44 +5,73 @@ import Form from 'react-bootstrap/Form';
 function SearchForm()
 {
     return (
-        <div className="d-flex flex-column justify-content-center align-items-center">
-            <div className="body-form-container flex-column w-25">
-                <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>State:</Form.Label>
-                        <Form.Control type="" placeholder="Enter State" />
-                    </Form.Group>
+        <div>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>License Plate:</Form.Label>
-                        <Form.Control type="" placeholder="License Plate Number" />
-                    </Form.Group>
+            <div className="d-flex flex-column justify-content-center align-items-center">
+                <div className="body-form-container flex-column w-25">
+                    <Form>
+                        <h1 className="text-center fw-bold">Report a Safety Issue</h1>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>State:</Form.Label>
+                            <Form.Control type="" placeholder="Enter State" />
+                        </Form.Group>
 
-                    <div className="d-flex justify-content-evenly">
-                        <div className="d-flex flex-column w-25">
-                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                <Form.Check className="body-form-checkbox" type="checkbox" label="Headlight" />
-                                <Form.Check className="body-form-checkbox" type="checkbox" label="Break Light" />
-                                <Form.Check className="body-form-checkbox" type="checkbox" label="Front Blinker" />
-                            </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>License Plate:</Form.Label>
+                            <Form.Control type="" placeholder="License Plate Number" />
+                        </Form.Group>
+
+                        <div className="d-flex justify-content-evenly">
+                            <div className="d-flex flex-column w-25">
+                                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                    <Form.Check className="body-form-checkbox" type="checkbox" label="Headlight" />
+                                    <Form.Check className="body-form-checkbox" type="checkbox" label="Break Light" />
+                                    <Form.Check className="body-form-checkbox" type="checkbox" label="Front Blinker" />
+                                </Form.Group>
+                            </div>
+
+                            <div className="d-flex flex-column w-25">
+                                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                    <Form.Check className="body-form-checkbox" type="checkbox" label="Tail Light" />
+                                    <Form.Check className="body-form-checkbox" type="checkbox" label="Reverse Light" />
+                                    <Form.Check className="body-form-checkbox" type="checkbox" label="Rear Blinker" />
+                                </Form.Group>
+                            </div>
                         </div>
-
-                        <div className="d-flex flex-column w-25">
-                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                                <Form.Check className="body-form-checkbox" type="checkbox" label="Tail Light" />
-                                <Form.Check className="body-form-checkbox" type="checkbox" label="Reverse Light" />
-                                <Form.Check className="body-form-checkbox" type="checkbox" label="Rear Blinker" />
-                            </Form.Group>
+                        <div className="d-flex flex-column">
+                            <Button className="btn btn-lg" variant="primary" type="submit">
+                                Submit
+                            </Button>
                         </div>
-                    </div>
-                    <div className="d-flex flex-column">
-                        <Button className="btn btn-lg" variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </div>
-                </Form>
+                    </Form>
+                </div>
+            </div>
+
+
+            <div className="d-flex flex-column justify-content-center align-items-center">
+                <div className="body-form-container flex-column w-25">
+                    <Form>
+                        <h1 className="text-center fw-bold">Check a License Plate</h1>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>State:</Form.Label>
+                            <Form.Control type="" placeholder="Enter State" />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>License Plate:</Form.Label>
+                            <Form.Control type="" placeholder="License Plate Number" />
+                        </Form.Group>
+
+                        <div className="d-flex flex-column">
+                            <Button className="btn btn-lg" variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </div>
+                    </Form>
+                </div>
             </div>
         </div>
+
 
     );
 }
