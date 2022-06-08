f28d3bde4bfc85df0019802503edb89a2e3939e0
ChrisShocker <fallenchris@live.com>
2022-06-08 12:30:12 -0700

    add bootstrap classes to center form

--- My Portfolio/Applications/React/Vehicle-Safety-Alert/client/src/components/body/Form.jsx
+++ My Portfolio/Applications/React/Vehicle-Safety-Alert/client/src/components/body/Form.jsx
@@ -6,7 +6,7 @@ function SearchForm()
 {
     return (
         <div className="d-flex flex-column justify-content-center align-items-center">
-            <div className="body-form-container flex-column w-50">
+            <div className="body-form-container flex-column w-25">
                 <Form>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>State:</Form.Label>
@@ -17,17 +17,29 @@ function SearchForm()
                         <Form.Label>License Plate:</Form.Label>
                         <Form.Control type="" placeholder="License Plate Number" />
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Headlight" />
-                        <Form.Check type="checkbox" label="Tailight" />
-                        <Form.Check type="checkbox" label="Break Light" />
-                        <Form.Check type="checkbox" label="Reverse Light" />
-                        <Form.Check type="checkbox" label="Front Blinker" />
-                        <Form.Check type="checkbox" label="Back Blinker" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
+
+                    <div className="d-flex justify-content-evenly">
+                        <div className="d-flex flex-column w-25">
+                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                <Form.Check className="body-form-checkbox" type="checkbox" label="Headlight" />
+                                <Form.Check className="body-form-checkbox" type="checkbox" label="Break Light" />
+                                <Form.Check className="body-form-checkbox" type="checkbox" label="Front Blinker" />
+                            </Form.Group>
+                        </div>
+
+                        <div className="d-flex flex-column w-25">
+                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                <Form.Check className="body-form-checkbox" type="checkbox" label="Tail Light" />
+                                <Form.Check className="body-form-checkbox" type="checkbox" label="Reverse Light" />
+                                <Form.Check className="body-form-checkbox" type="checkbox" label="Rear Blinker" />
+                            </Form.Group>
+                        </div>
+                    </div>
+                    <div className="d-flex flex-column">
+                        <Button className="btn btn-lg" variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </div>
                 </Form>
             </div>
         </div>
